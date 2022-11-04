4b06c719c4a3fc04aade07352f24d6aed228f0ec
Ha Young Roh <hyroh795@gmail.com>
2022-11-03 20:06:26 -0400

    commit tree e63aa513df46c79b2fd52d4839a592afb3c154db

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(array) {
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function (fare) { return fare*int;};
+}
+
+const before = createFareMultiplier();
+function fareDoubler(before) {
+
+    return before*2;
+}
+
+const before2 = createFareMultiplier();
+
+function fareTripler(before2) {
+    return before2*3;
+}
+
+
+function selectDifferentDrivers(arrayOfDrivers, functionType) {
+   return functionType(arrayOfDrivers);
+}
